53a5937fc202304fb316f2e76b43457eb85d8a87
Adam Due Hansen <adamduehan@gmail.com>
2024-12-06 23:28:48 +0100

    feat: 'Index all' button is disabled during load.

--- islands/new-index-button.tsx
+++ islands/new-index-button.tsx
@@ -1,14 +1,27 @@
 import { JSX } from "preact/jsx-runtime";
+import { useState } from "preact/hooks";
 import { usePages } from "$islands/PagesContext.tsx";
 
 export function NewIndexButton(): JSX.Element {
+  const [pending, setPending] = useState<"error" | "idle" | "loading">("idle");
   const { setPages } = usePages();
 
   async function startIndex() {
-    const response = await fetch("/api/index-sitemap-database");
-    const json = await response.json();
-    setPages(json);
+    try {
+      setPending("loading");
+      const response = await fetch("/api/index-sitemap-database");
+      const json = await response.json();
+      setPages(json);
+      setPending("idle");
+    } catch (error) {
+      setPending("error");
+      throw error;
+    }
   }
 
-  return <button onClick={startIndex}>New index!</button>;
+  return (
+    <button disabled={pending === "loading"} onClick={startIndex}>
+      New index!
+    </button>
+  );
 }
