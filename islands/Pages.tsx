66e6a59882702a98dbba19c6c0721332e7778038
Adam Due Hansen <adamduehan@gmail.com>
2024-12-12 22:14:17 +0100

    feat: EventSource is opened from client on load.

--- islands/Pages.tsx
+++ islands/Pages.tsx
@@ -1,23 +1,53 @@
 import { JSX } from "preact/jsx-runtime";
+import { useEffect } from "preact/hooks";
 import { PageDetails } from "$islands/PageDetails.tsx";
 import { usePages } from "$islands/PagesContext.tsx";
+import { Page } from "$utils/page.ts";
 
 export function Pages(): JSX.Element {
-  const { pages, updatePage } = usePages();
+  const { pages, updatePage, setPages } = usePages();
+
+  useEffect(() => {
+    const eventSource = new EventSource(globalThis.location.href);
+    eventSource.addEventListener("open", () => {
+      console.log("Connection open!");
+    });
+    eventSource.addEventListener("message", (ev) => {
+      console.log("Message", ev);
+      const data: Page[] = JSON.parse(ev.data);
+      setPages(data);
+    });
+    eventSource.addEventListener("error", (ev) => {
+      console.log("Error", ev);
+    });
+  }, []);
+
+  const handler: JSX.SubmitEventHandler<HTMLFormElement> = function () {
+  };
 
   return (
-    <div>
-      <p>{pages.length} pages found in sitemap</p>
-      {pages.map((page) => {
-        return (
-          <PageDetails
-            page={page}
-            onUpdated={(updatedPage) => {
-              updatePage(updatedPage.id, updatedPage);
-            }}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div>
+        <p>{pages.length} pages found in sitemap</p>
+        <p>{pages.filter((page) => page.status === 200).length} are "ok"</p>
+      </div>
+
+      <form onSubmit={handler}>
+        <button>Index all</button>
+      </form>
+
+      <div>
+        {pages.map((page) => {
+          return (
+            <PageDetails
+              page={page}
+              onUpdated={(updatedPage) => {
+                updatePage(updatedPage.id, updatedPage);
+              }}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
--- routes/[indexId].tsx
+++ routes/[indexId].tsx
@@ -3,6 +3,7 @@ import { Pages } from "$islands/Pages.tsx";
 import { PagesProvider } from "$islands/PagesContext.tsx";
 import { Page } from "$utils/page.ts";
 import { getIndex } from "$utils/db.ts";
+import { db } from "$services/db.ts";
 
 interface Props {
   indexTimestamp: number;
@@ -10,8 +11,50 @@ interface Props {
 }
 
 export const handler: Handlers<Props> = {
-  GET: async function (_req, ctx) {
-    const index = await getIndex(ctx.params.indexId);
+  GET: async function (req, ctx) {
+    const indexId = ctx.params.indexId;
+
+    if (req.headers.get("accept") === "text/event-stream") {
+      const stream = db.watch([["page", indexId]]).getReader();
+      const body = new ReadableStream({
+        start: async function (controller) {
+          console.log(
+            `Opened stream for list ${indexId} remote ${
+              JSON.stringify(ctx.remoteAddr)
+            }`,
+          );
+          while (true) {
+            try {
+              if ((await stream.read()).done) {
+                return;
+              }
+
+              const data = await getIndex(indexId);
+              const chunk = `data: ${JSON.stringify(data.pages)}\n\n`;
+              controller.enqueue(new TextEncoder().encode(chunk));
+            } catch (err) {
+              console.error(err);
+            }
+          }
+        },
+        cancel() {
+          stream.cancel();
+          console.log(
+            `Closed stream for list ${indexId} remote ${
+              JSON.stringify(ctx.remoteAddr)
+            }`,
+          );
+        },
+      });
+
+      return new Response(body, {
+        headers: {
+          "Content-Type": "text/event-stream",
+        },
+      });
+    }
+
+    const index = await getIndex(indexId);
 
     return ctx.render({
       indexTimestamp: index.timestamp,
